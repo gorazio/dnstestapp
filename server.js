fb89992e779b46ef560fb6e8bc5ccd71c338eeb7
Evgeny Kruglov <ekruglov@gmail.com>
2016-05-17 17:47:52 +0200

    initial

---
+++ server.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const dns=require('dns');
+
+let requests=0;
+
+setTimeout(function callMe() {
+  let times = {};
+  let done=0;
+
+  for (let i = 0; i < 20; i++) {
+    (function (iter) {
+      times[iter] = Date.now();
+      dns.lookup('ngx.discovery', {all: 1}, function (err,ips) {
+        let timestamp=Date.now();
+        process.nextTick(()=>{
+          requests++;
+          if (timestamp-times[iter]>1000){
+            console.log(`Resolving takes ${timestamp-times[iter]}ms after ${requests} requests`)
+          }
+          if (!ips.startsWith('10.0.')){
+            console.log(`Wrong ip returned: ${ips}`)
+          }
+
+          if (!--done){
+            setImmediate(callMe);
+          }
+
+        });
+      });
+      done++;
+    })(i);
+  }
+});
